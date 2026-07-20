9dd5a3d7cfeb1871e1dd7679862f0d779a9ff97a
Indrajeet <indrajeet20mandal02@gmail.com>
2026-07-20 22:19:27 +0530

    Time: 0 ms (100%), Space: 8.3 MB (42.95%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     inline static int q[45];
+
     inline static bool init = []() {
         int n = 0;
         for (int i = 1; i < 10; i++)
             q[n++] = i;
-
         for (int i = 0; i < n; i++) {
             int d = q[i] % 10;
             if (d < 9) q[n++] = q[i] * 10 + d + 1;
         }
-
         return 0;
     }();
 
