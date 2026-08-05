77d71225ba10a9f3299e18b7b0fb4cfada520514
Indrajeet <indrajeet20mandal02@gmail.com>
2026-08-05 15:16:56 +0530

    Time: 166 ms (54.01%), Space: 17.6 MB (60.08%) - LeetHub

--- 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.cpp
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.cpp
@@ -1,68 +1,25 @@
 class Solution {
 public:
     int numberOfStableArrays(int zero, int one, int limit) {
-        long long MOD = 1000000007;
-        int maxN = zero + one;
-        
-        std::vector<long long> fact(maxN + 1, 0);
-        std::vector<long long> invFact(maxN + 1, 0);
-        
-        fact[0] = 1;
-        invFact[0] = 1;
-        for (int i = 1; i <= maxN; i++) {
-            fact[i] = (fact[i - 1] * i) % MOD;
-        }
-        
-        auto power = [&](long long baseVal, long long exp) {
-            long long res = 1;
-            baseVal %= MOD;
-            while (exp > 0) {
-                if (exp & 1) res = (res * baseVal) % MOD;
-                baseVal = (baseVal * baseVal) % MOD;
-                exp >>= 1;
-            }
-            return res;
-        };
-        
-        invFact[maxN] = power(fact[maxN], MOD - 2);
-        for (int i = maxN - 1; i >= 1; i--) {
-            invFact[i] = (invFact[i + 1] * (i + 1)) % MOD;
-        }
-        
-        auto C = [&](int n, int k) -> long long {
-            if (k < 0 || k > n) return 0;
-            return fact[n] * invFact[k] % MOD * invFact[n - k] % MOD;
-        };
-        
-        auto F = [&](int N, int K, int L) -> long long {
-            if (K <= 0 || K > N) return 0;
-            long long ans = 0;
-            int maxJ = (N - K) / L;
-            for (int j = 0; j <= maxJ; j++) {
-                long long term = C(K, j) * C(N - j * L - 1, K - 1) % MOD;
-                if (j & 1) {
-                    ans = (ans - term + MOD) % MOD;
-                } else {
-                    ans = (ans + term) % MOD;
-                }
-            }
-            return ans;
-        };
+        const int mod = 1'000'000'007;
 
-        int maxK = std::min(zero, one + 1);
-        std::vector<long long> fOne(maxK + 2, 0);
-        for (int k = 1; k <= maxK + 1; k++) {
-            fOne[k] = F(one, k, limit);
+        vector<vector<int>> dp0(zero + 1, vector<int>(one + 1, 0)); // i 0s + j 1s ending with 0
+        vector<vector<int>> dp1(zero + 1, vector<int>(one + 1, 0)); // i 0s + j 1s ending with 1
+
+        // Base cases: only zeros or only ones => only one string if len <= min(limit, zero/one)
+        for (int i = 1; i <= min(zero, limit); ++i) dp0[i][0] = 1;
+        for (int j = 1; j <= min(one, limit); ++j) dp1[0][j] = 1;
+
+        // DP iterations
+        for (int i = 1; i <= zero; ++i) {
+            for (int j = 1; j <= one; ++j) {
+                for (int k = 1; k <= min(limit, i); ++k)
+                    dp0[i][j] = (dp0[i][j] + dp1[i - k][j]) % mod;
+                for (int k = 1; k <= min(limit, j); ++k)
+                    dp1[i][j] = (dp1[i][j] + dp0[i][j - k]) % mod;
+            }
         }
-        
-        long long ans = 0;
-        for (int k = 1; k <= maxK; k++) {
-            long long fz = F(zero, k, limit);
-            if (fz == 0) continue;
-            long long fo = (fOne[k - 1] + 2 * fOne[k] + fOne[k + 1]) % MOD;
-            ans = (ans + fz * fo) % MOD;
-        }
-        
-        return static_cast<int>(ans);
+
+        return (dp0[zero][one] + dp1[zero][one]) % mod;
     }
 };
